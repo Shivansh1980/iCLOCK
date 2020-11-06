0090e6b4eee87fd71a9df20293c874c6324c6353
Shivansh <shivanshshrivastava1980@gmail.com>
2020-11-05 16:03:27 -0800

    updated book detail section

--- app/src/main/java/com/example/iclock/BookDetails.java
+++ app/src/main/java/com/example/iclock/BookDetails.java
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BookDetails extends Fragment {
+import com.example.iclock.dummy.CreateBook;
 
+public class BookDetails extends Fragment {
+    CreateBook createBook;
     public BookDetails() {
         // Required empty public constructor
     }
@@ -23,6 +25,14 @@ public class BookDetails extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_book_details, container, false);
+
+        //The below createBook object contains the detail of Books so make a layout and show all the details
+        //Assume this createBook object contains all details of book that are available in CreateBook class
+        //you can get details like this createBook.getBookName(); this will give you book name and now on the
+        //layout you can setText it.
+        Bundle bundle = getArguments();
+        createBook = (CreateBook) bundle.getSerializable("BookDetails");
+
         return root;
     }
 }
